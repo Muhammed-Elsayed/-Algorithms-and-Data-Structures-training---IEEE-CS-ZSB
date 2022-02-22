118e3395ad246513027dcb9ca337086d7fedb077
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-02-22 23:50:32 +0200

    Create pset1.py

---
+++ task1/pset1.py
@@ -0,0 +1,10 @@
+length_in_cm = int(input())
+
+if length_in_cm <= 800:
+    print("1")
+
+elif length_in_cm <= 1400:
+    print("2")
+
+elif length_in_cm > 1400 and length_in_cm <= 2000:
+    print("3")
