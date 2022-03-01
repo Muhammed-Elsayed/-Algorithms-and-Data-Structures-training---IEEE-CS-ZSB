84e30d4a89283b948d082d4c9b862eadc59040a5
mohamed elsayed <70416898+mo6502@users.noreply.github.com>
2022-03-01 23:23:59 +0200

    Update pset8.py

--- task2/pset8.py
+++ task2/pset8.py
@@ -1,3 +1,23 @@
+
+# a code that runs 
+difficulty = list(map(int, input().split()))[1]
+list1 = list(map(int, input().split()))
+counter = 0
+the_left = 0
+the_right = len(list1) - 1
+
+while the_left - the_right != 1:
+    if list1[the_left] <= difficulty:
+        counter += 1
+        the_left += 1
+    elif list1[the_right] <= difficulty:
+        counter += 1
+        the_right -= 1
+    else:
+        break
+print(counter)
+
+
 # with run time error
 
 x, difficulty = input().split()
@@ -19,3 +39,4 @@ if counter != len(list1):
             break
 
 print(counter)
+
